1c131e91c977b317d5e23eaeb36ee07abce883af
dacalonew <cowand@oregonstate.edu>
2017-03-02 16:20:56 -0800

    Adding all the images and changed the code to plot spacially resolved, and much much more...

--- ME552_skelatel_Code_W17.m
+++ ME552_skelatel_Code_W17.m
@@ -28,34 +28,35 @@ fg_path = {'A2_5k_HTI_01.tiff','A2_5k_HTI_02.tiff','A2_5k_HTI_03.tiff',...
 m_dot_total =0.001215;               %Total mass flow (kg/s) of reactants for Re=5,000
 %m_dot_total=.00243;                 %Total mass flow (kg/s) of reactants for Re=10,000
 rho_u       = 0.7871;                     %Unburned Density (kg/m^3) at 200 C
-D           = 12;                       %Burner Diameter
+D           = 12/10;                       %Burner Diameter (cm)
 
-%% Image Parameters - Selected From Image
-%Use imread, imshow, and ginput functions to complete calibration
-%You will need to calibrate to the burner width, the outlet plane of the
-%burner, the top of the image, and the number of pixels per unit length 
-%(mm or in)
+%%  Spacial Calibration
+
 %This will be used for image croping and spatial calibration
 info_cal= imfinfo(Cal_path);
 
+im_cal=imread(Cal_path,1,'Info',info_cal,'PixelRegion',{[775,921],[300,900]});
+figure(15)
+imshow(im_cal,[50,230]); axis image %This will show the image to calibrate to
+title('Select locations 70cm and 76cm') %Please select locations 70cm and 76cm
+points=ginput(2);
+close
+point_dist=pdist(points,'euclidean');
+cal_val=6/point_dist; %Image calibration  (cm/pixel)
+
+%% Image Cropping
+
 center_l=514;                       %axis of symetry
-Lcrop= 349;                       % left edge of the Burner
+Lcrop= 440;                       % left edge of the Burner
 Rcrop= 1024+Lcrop-2*center_l;                       % right edge of the Burner
-Tcrop= 200;                       % location for cropping of the top of the image
-%Tcrop_LRe= 100;                       % location for cropping of the top of the image
-%Tcrop_SRe= 500;                       % location for cropping of the top of the image
-Bcrop= 1024-900;                       % location for cropping of the bottom of the image
+Tcrop= 400;                       % location for cropping of the top of the image
+Bcrop= 1024-921;                       % location for cropping of the bottom of the image
 
-
+%% Background images
 
 for f=1:length(bg_path) %finding backgound info for both Re
     info_b=imfinfo(bg_path{f}); %info of image
-%     if f==1 %This creates the two different crops for both Re values
-%         Tcrop=Tcrop_SRe;
-%     else
-%         Tcrop=Tcrop_LRe;
-%     end
-    for i=1:1 %calculating the background average
+    for i=1:numframes %calculating the background average
         if i==1
             im_avg_b=double(imread(bg_path{f},1,'Info',info_b,'PixelRegion',{[Tcrop,1024-Bcrop],[Lcrop,1024-Rcrop]}));
         else
@@ -66,79 +67,364 @@ for f=1:length(bg_path) %finding backgound info for both Re
     b_avg(:,:,f)=im_avg_b; %This has both Re values in it.
 end
 
-figure()
-for f=1:length(fg_path)
-    info_f=imfinfo(fg_path{f}); %info of image
-%     if f<6.5 %This creates the two different crops for both Re values
-%         Tcrop=Tcrop_SRe;
-%     else
-%         Tcrop=Tcrop_LRe;
-%     end
+%% Building Meshgrid 
 
-    for i=1:1 %calculating the background average
+siz=size(im_avg_b); %Used to greate mesh grid
+x=0:(siz(2)-1); 
+x=cal_val*x; %Calibrated x distance
+y=0:(siz(1)-1); 
+y=cal_val*y; %Calibrated y distance
+y=fliplr(y);
+
+[X,Y]=meshgrid(x,y);
+%Y=flipud(Y);
+
+%% Calculating averages and plotting images
+
+count=1;
+for f=1:length(fg_path) %Plotting all files
+    info_f=imfinfo(fg_path{f}); %info of image
+
+
+    for i=1:numframes %calculating the image average
         if i==1
-            im_avg_f=double(imread(fg_path{f},1,'Info',info_f,'PixelRegion',{[Tcrop,1024-Bcrop],[Lcrop,1024-Rcrop]}));
+            image_0=double(imread(fg_path{f},1,'Info',info_f,'PixelRegion',{[Tcrop,1024-Bcrop],[Lcrop,1024-Rcrop]}));
+            im_avg_f=image_0;
         else
             image_i=double(imread(fg_path{f},i,'Info',info_f,'PixelRegion',{[Tcrop,1024-Bcrop],[Lcrop,1024-Rcrop]}));
             im_avg_f=((i-1).*im_avg_f+image_i)./i;
         end
     end
-    f_avg(:,:,f)=im_avg_f; %This has both Re values in it.
+    
+    f_avg(:,:,f)=im_avg_f; %Image average per test. This has both Re values in it.
+    max_int=90;
     
     if f<6.5 %The following is calculating the flame minus the background
         flame_avg(:,:,f)=f_avg(:,:,f)-b_avg(:,:,1);
     else
         flame_avg(:,:,f)=f_avg(:,:,f)-b_avg(:,:,2);
     end
-    max_int=max(max(flame_avg(:,:,f))); %finding maxinum intensity for plot reference
-    axis_sym=0.5*(flame_avg(:,1:end,f) + flame_avg(:, end+1-(1:end), f));   % Axis symetric
-    flame(:,:,f)=medfilt2(uint8(axis_sym)); %filters and turns back into image
     
-    if f<3.5
-        figure(1)
-        subplot(1,3,f)
-        imshow(flame(:,:,f),[0,max_int])
-        if f==2
-            title('Axis Symetric Corrected Flame (Re=5,000, High Turbulence Intensity)')
+%    max_int=max(max(flame_avg(:,:,f))); %finding maxinum intensity for per test
+    flame_filt(:,:,f)=medfilt2(flame_avg(:,:,f)); %filters
+    flame(:,:,f)=0.5*(flame_avg(:,1:end,f) + flame_filt(:, end+1-(1:end), f));   % Axis symetric
+
+    
+    if rem(f,3)==0 %This is to find the average with all three tests
+        avg_flame(:,:,count)=mean(flame(:,:,f-2:f),3);
+        if f<3.5 %Plotting average to compare to snap-shots
+            figure(1)
+            subplot(1,7,1)
+            colormap jet
+            surf(X,Y,avg_flame(:,:,count),'EdgeColor','None'); axis image
+            caxis([0,65])
+            view(2)
+            xlim([0,max(x)])
+            ylim([0,max(y)])
+            title('Axis Symetric Corrected Flame (Re=5,000, High Turbulence Intensity)','HorizontalAlignment', 'left')
+            xlabel('X-Distance (cm)')
+            ylabel('Y-Distance (cm)')
+        elseif f<6.5
+            figure(2)
+            subplot(1,7,1)
+            colormap jet
+            surf(X,Y,avg_flame(:,:,count),'EdgeColor','None'); axis image
+            caxis([0,65])
+            view(2)
+            grid off
+            xlim([0,max(x)])
+            ylim([0,max(y)])
+            title('Axis Symetric Corrected Flame (Re=5,000, Low Turbulence Intensity)','HorizontalAlignment', 'left')
+            xlabel('X-Distance (cm)')
+            ylabel('Y-Distance (cm)')
+        elseif f<9.5
+            figure(3)
+            subplot(1,7,1)
+            colormap jet
+            surf(X,Y,avg_flame(:,:,count),'EdgeColor','None'); axis image
+            caxis([0,65])
+            view(2)
+            grid off
+            xlim([0,max(x)])
+            ylim([0,max(y)])
+            title('Axis Symetric Corrected Flame (Re=10,000, High Turbulence Intensity)','HorizontalAlignment', 'left')
+            xlabel('X-Distance (cm)')
+            ylabel('Y-Distance (cm)')
+        else
+            figure(4)
+            subplot(1,7,1)
+            colormap jet
+            surf(X,Y,avg_flame(:,:,count),'EdgeColor','None'); axis image
+            caxis([0,65])
+            view(2)
+            grid off
+            xlim([0,max(x)])
+            ylim([0,max(y)])
+            title('Axis Symetric Corrected Flame (Re=10,000, Low Turbulence Intensity)','HorizontalAlignment', 'left')
+            xlabel('X-Distance (cm)')
+            ylabel('Y-Distance (cm)')
         end
-        %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
-        xlabel('X-Distance (cm)')
-        ylabel('Y-Distance (cm)')
+        count=count+1;
+    end
+    
+    title_loc=4; %Location of center of title over which sub plot
+    
+    if f<3.5 %The following if statement plots the 6 snap-shots
+        figure(1)
+        a=f;
+        subplot(1,7,a+1)
+        colormap jet
+        surf(X,Y,image_0-b_avg(:,:,1),'EdgeColor','None'); axis image %one snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
+        subplot(1,7,a+4)
+        colormap jet
+        surf(X,Y,image_i-b_avg(:,:,1),'EdgeColor','None'); axis image %The other snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
     elseif f<6.5
         figure(2)
-        subplot(1,3,f-3)
-        imshow(flame(:,:,f),[0,max_int])
-        if f==5
-            title('Axis Symetric Corrected Flame (Re=5,000, Low Turbulence Intensity)')
-        end
-        %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
-        xlabel('X-Distance (cm)')
-        ylabel('Y-Distance (cm)')
+        a=f-3;
+        subplot(1,7,a+1)
+        colormap jet
+        surf(X,Y,image_0-b_avg(:,:,1),'EdgeColor','None'); axis image %one snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
+        subplot(1,7,a+4)
+        surf(X,Y,image_i-b_avg(:,:,1),'EdgeColor','None'); axis image %The other snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
     elseif f<9.5
         figure(3)
-        subplot(1,3,f-6)
-        imshow(flame(:,:,f),[0,max_int])
-        if f==8
-            title('Axis Symetric Corrected Flame (Re=10,000, High Turbulence Intensity)')
-        end
-        %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
-        xlabel('X-Distance (cm)')
-        ylabel('Y-Distance (cm)')
+        a=f-6;
+        subplot(1,7,a+1)
+        colormap jet
+        surf(X,Y,image_0-b_avg(:,:,2),'EdgeColor','None'); axis image %one snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
+        subplot(1,7,a+4)
+        colormap jet
+        surf(X,Y,image_i-b_avg(:,:,2),'EdgeColor','None'); axis image %The other snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
     else
         figure(4)
-        subplot(1,3,f-9)
-        imshow(flame(:,:,f),[0,max_int])
-        if f==11
-            title('Axis Symetric Corrected Flame (Re=10,000, Low Turbulence Intensity)')
-        end
-        %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
-        xlabel('X-Distance (cm)')
-        ylabel('Y-Distance (cm)')
+        a=f-9;
+        subplot(1,7,a+1)
+        colormap jet
+        surf(X,Y,image_0-b_avg(:,:,2),'EdgeColor','None'); axis image %one snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
+        subplot(1,7,a+4)
+        colormap jet
+        surf(X,Y,image_i-b_avg(:,:,2),'EdgeColor','None'); axis image %The other snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        set(gca,'ytick',[])
+        
+    end
+    
+    if f<3.5 %This plots average per test. Set to comment because not needed
+%         figure(1)
+%         a=f;
+%         subplot(1,3,a)
+%         surf(X,Y,flame(:,:,f),'EdgeColor','None')
+%         view(2)
+%         grid off
+%         xlim([0,max(x)])
+%         ylim([0,max(y)])
+%         %imshow(flame(:,:,f),[0,max_int])
+%         if a==title_loc 
+%             title('Axis Symetric Corrected Flame (Re=5,000, High Turbulence Intensity)')
+%         end
+%         %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
+%         xlabel('X-Distance (cm)')
+%         ylabel('Y-Distance (cm)')
+%     elseif f<6.5
+%         figure(2)
+%         a=f-3;
+%         subplot(1,3,a)
+%         surf(X,Y,flame(:,:,f),'EdgeColor','None')
+%         view(2)
+%         grid off
+%         xlim([0,max(x)])
+%         ylim([0,max(y)])
+%         %imshow(flame(:,:,f),[0,max_int])
+%         if a==title_loc
+%             title('Axis Symetric Corrected Flame (Re=5,000, Low Turbulence Intensity)')
+%         end
+%         %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
+%         xlabel('X-Distance (cm)')
+%         ylabel('Y-Distance (cm)')
+%     elseif f<9.5
+%         a=f-6;
+%         subplot(1,3,a)
+%         surf(X,Y,flame(:,:,f),'EdgeColor','None')
+%         view(2)
+%         grid off
+%         xlim([0,max(x)])
+%         ylim([0,max(y)])
+%         %imshow(flame(:,:,f),[0,max_int])
+%         if a==title_loc
+%             title('Axis Symetric Corrected Flame (Re=10,000, High Turbulence Intensity)')
+%         end
+%         %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
+%         xlabel('X-Distance (cm)')
+%         ylabel('Y-Distance (cm)')
+%     else
+%         figure(4)
+%         a=f-9;
+%         subplot(1,3,a)
+%         surf(X,Y,flame(:,:,f),'EdgeColor','None')
+%         view(2)
+%         grid off
+%         xlim([0,max(x)])
+%         ylim([0,max(y)])
+%         %imshow(flame(:,:,f),[0,max_int])
+%         if a==title_loc
+%             title('Axis Symetric Corrected Flame (Re=10,000, Low Turbulence Intensity)')
+%         end
+%         %legend('L/e=2e3','L/e=5e3','L/e=1e4','L/e=5e4')
+%         xlabel('X-Distance (cm)')
+%         ylabel('Y-Distance (cm)')
     end
 
 end
 
-% for x=1:6 %This was used to find the crop values for centering image
+%% Finding flame area
+center=(siz(2)+1)/2; %center index
+%figure(11)
+%plot(y,movmean(avg_flame(:,center,2),20))
+%plot(h,max1h)
+
+count=zeros(1,4);
+for f=1:4
+    for i=1:siz(2)
+        for j=1:siz(1)
+            if avg_flame(j,i,f)>24
+                count(f)=count(f)+1;
+            end
+        end
+    end
+end
+appearent_flame_A=cal_val^2*count;
+
+
+for i=1:4 %This is old incorrect plotting
+    [val,indx]=max(movmean(avg_flame(:,center,i),20)); %Locates max temp along center
+    height(i)=y(indx); %Finds height of cone
+    
+    cone_A(i)=(D/2)*pi()*(height(i)^2+(D/2)^2)^(1/2); %cone area
+
+end
+
+
+%% Additional Plots
+
+plt_titles={'Re=5k HTI','Re=5k LTI','Re=10k HTI','Re=10k LTI'};
+%method_plts=zeros(5,siz(1),siz(2),4);
+for f=1:4 %This shows all the flames under all four conditions
+    figure(5)
+    subplot(1,4,f)
+    surf(X,Y,avg_flame(:,:,f),'EdgeColor','None'); axis image %one snap shot per test
+    colormap jet
+    caxis([0,65])
+    view(2)
+    xlim([0,max(x)])
+    ylim([0,max(y)])
+    title(plt_titles{f})
+    if f==1
+        ylabel('Y-Distance (cm)')
+    elseif f==2
+        xlabel('X-Distance (cm)')
+        set(gca,'ytick',[])
+    else
+        set(gca,'ytick',[])
+    end
+    
+    a=f*3;
+    b=ceil(f/2); %The method_plts will show the method
+    method_plts(:,:,:,f)=cat(3,b_avg(:,:,b),f_avg(:,:,a),flame_avg(:,:,a),flame_filt(:,:,a),flame(:,:,a));
+    
+end
+
+plt_titles={'Re=5,000, High Turbulence Intensity',...
+    'Re=5,000, Low Turbulence Intensity',...
+    'Re=10,000, High Turbulence Intensity',...
+    'Re=10,000, Low Turbulence Intensity'};
+plt_xlab={'Background','Average Flame','Background Subtracted','Filtered','Axis Symetric'};
+for f=1:4
+    figure()
+    for i=1:5
+        subplot(1,5,i)
+        colormap jet
+        surf(X,Y,method_plts(:,:,i,f),'EdgeColor','None'); axis image %one snap shot per test
+        caxis([0,65])
+        view(2)
+        xlim([0,max(x)])
+        ylim([0,max(y)])
+        if i==1
+            title(plt_titles{f},'HorizontalAlignment', 'left')
+            ylabel('Y-Distance (cm)')
+            %axes('Position', [0.05 0.05 0.9 0.9], 'Visible', 'off');
+            %colorbar
+        else
+            set(gca,'ytick',[])
+        end
+        xlabel(plt_xlab{i});
+        set(get(gca,'XLabel'),'Rotation',20,'HorizontalAlignment', 'right')
+    end
+end
+        
+
+%% Color Bar Graph
+x_cb=0:65;
+y_cb=0:5;
+[X_cb,Y_cb]=meshgrid(x_cb,y_cb);
+figure()
+colormap jet
+surf(X_cb,Y_cb,X_cb,'EdgeColor','None'); axis image %one snap shot per test
+caxis([0,65])
+view(2)
+set(gca,'ytick',[])
+xlabel('Intensity')
+
+
+
+
+%% Was used to find Flame center
+
+for x=1:6 %This was used to find the crop values for centering image
 %     max1h=max(flame_avg(:,:,x));
 %     max2h=max(flame_avg(:,:,x+6));
 % 
@@ -155,39 +441,6 @@ end
 %     plot(v,max2v)
 %     %plot(h,max2h)
 %     hold on
-% end
-
-% flame_avg=im_avg_f-im_avg_b;
-% max_f=max(max(flame_avg(1:6)));
-% flame=uint8(flame_avg);
-% figure, imshow(flame,[0,max_f])
-
-% 
-% Left                    = ;                                                     % location of left line on calibration device
-% Right                   = ;                                                     % location of right line on calibration device
-% 
-% %% Image Parameters - User Selected
-% %calibration determination
-% Cal_Length              = ;                                                     % Calibration Unit Length
-% pixels                  = Right - Left;                                         % # of Pixels
-% Calibration             = Cal_Length/pixels;                                    % meters/pixel
-% 
-% %% Image Read In, Average, and Background Subtract
-% %read on .tiff, average images in multipage format for backgrounds
-% %subtract background form images in foreground
-
-% 
-% %% Axisymmetrize the image by averaging both sides
-% Axisym = 0.5*(cropped_Flame(:, 1:end) + cropped_Flame(:, end + 1 - (1:end)));   % Use this equation to correct for asymetry
-% [f, g] = size(Axisym(:, :));                                                    % get dimensions of the axisymmetrized image
-% 
-% %% Perform 2D median filter on images
-% ImageFilt(:, :) = ;                                                             %
-% 
-% %% This is were I leave you
-% % you will need to develope a method to reduce your data further to
-% % calcutate the stated quantities in the lab procedure.
-% % One last note,
-% % It's dangerous to go alone, take this https://www.mathworks.com/help/index.html
+end
 
 
